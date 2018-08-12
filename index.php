13a4c385b51f519b12944edc24c31f25f0cd1bbb
Rahadur <get.rahadur@gmail.com>
2018-08-12 17:03:31 +0600

    modify index.php

--- index.php
+++ index.php
@@ -15,4 +15,31 @@ require 'database.php';
 
 
 
-var_dump($_POST);
+if ($_SERVER['REQUEST_METHOD'] == 'post') {
+
+    if (empty($_POST['chap-id'])) {
+        header('Location: '. ROUTER_ADDRESS);
+    }elseif (empty($_POST['chap-challenge'])) {
+        header('Location: '. ROUTER_ADDRESS);
+    }elseif (empty($_POST['mac'])){
+        header('Location: '. ROUTER_ADDRESS);
+    }elseif ($_POST['ip']){
+        header('Location: '. ROUTER_ADDRESS);
+    }elseif ($_POST['mac']){
+        header('Location: '. ROUTER_ADDRESS);
+    }else {
+
+        $_SESSION['router'] = $_POST;
+
+        header('Location: login.php');
+    }
+
+} else {
+
+    /**
+     * If request is not a post request redirect to router public IP address.
+     * in our case 192.168.2.1
+     */
+    return header('Location: '. ROUTER_ADDRESS);
+
+}
